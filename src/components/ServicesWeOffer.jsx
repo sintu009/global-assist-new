3cf72bb9cf2c4bb1b7f07c308f0d4c4743b65dc0
Ronit Singh Nayal <ronitsinghnayal@gmail.com>
2025-07-15 00:13:05 +0530

    changes

--- src/components/ServicesWeOffer.jsx
+++ src/components/ServicesWeOffer.jsx
@@ -134,7 +134,7 @@ const ServicesWeOffer = () => {
 
           </div>
 
-          <div className="absolute bottom-0 left-1/2 -translate-x-1/2 z-10 w-[220px] md:w-[450px] pointer-events-none hidden md:block">
+          <div className="absolute bottom-0 left-1/2 -translate-x-1/2 z-10 w-[230px] md:w-[450px] pointer-events-none  md:block">
             <img
               src={adminImage}
               alt={titles[index].big}
